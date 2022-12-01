4c107e86ad6cdf83be96710eb21eebc3677d1e95
erikybaltk <115509878+erikybaltk@users.noreply.github.com>
2022-12-01 15:54:55 -0300

    Add files via upload
    COMANDOS USADOS SQL

---
+++ DADOS SQL.sql
@@ -0,0 +1,13 @@
+-- Inserçoes das tabelas
+-- INSERT INTO [nome da tabela] ([colunas]) VALUES ()
+
+INSERT INTO tb_Produtos VALUES ('Torta de Chocolate', 4.5);
+-- ORDEM ERRADA
+
+-- INSERT INTO tb_Produtos VALUES (10.5, 'Bolo de Morango'); 
+-- Com os nomes das colunas
+INSERT INTO tb_Produtos (vlProduto, nmProduto) VALUES (10.5, 'Bolo de Morango');
+
+
+--Testando o Constraint
+INSERT INTO tb_Vendas VALUES(5, 3.2)
